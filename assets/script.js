8473d0d04173e60a4da582a9046adcd25cf506c1
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-04-06 11:37:28 -0500

    adding work done from second computer as it does not appear to have saved correctly

--- assets/script.js
+++ assets/script.js
@@ -2,55 +2,49 @@
 
 
 
-
+//Welcome page with message and start button
 document.querySelector("#startButton").addEventListener("click", function () {
   document.querySelector("#welcomeDiv").classList.add("hide");
-});
-
-document.querySelector("#questionsDiv")
-var currentQuestion = 0
-var timer = 75
-var answers = questions[currentQuestion].answers
-var questions = [
-    {text: "what is the capital of north korea?",
-    answers: ["pyongyang","tokyo"]
-    correctAnswer: "pyongyang"},
-    {text: "what is the capital of japan?",
-    answers: ["pyongyang","tokyo"]
-    correctAnswer: "tokyo"},
-    {text: "what is the capital of north korea?",
-    answers: ["pyongyang","tokyo"]
-    correctAnswer: "pyongyang"},
-    {text: "what is the capital of japan?",
-    answers: ["pyongyang","tokyo"]
-    correctAnswer: "tokyo"},
-]
-
-
-/*Welcome Page
-welcome text with start button
-start button clears page questions begin and timer starts
-questions require answer buttons
-wrong answers take away time
-right answers populate new question while time continues
-array of answers
-
-questions are gonna be in an array of objects.
-
-
-
-for loop to create buttons:
-
-for (for loop shit)
-document.querySelector(#answer1).innerHTML += "<button>"+answers[i]+"</button>"
-innerHTML = ""
-
-
-<whatever></whatever>
-
-when submitted, if answer correct, currentQuestion++
-if wrong timer -10
-
-
-
-*/
+  });
+  
+  //Q&A page
+  document.querySelector("#questionsDiv")
+  var currentQuestion = 0
+  var timer = 75
+  var answers = questions[currentQuestion].answers
+  var correctAnswer = questions[currentQuestion].correctAnswer
+  var questions = [
+      {text: "what is the capital of north korea?",
+      answers: ["pyongyang","tokyo"]
+      correctAnswer: "pyongyang"},
+      {text: "what is the capital of japan?",
+      answers: ["pyongyang","tokyo"]
+      correctAnswer: "tokyo"},
+      {text: "what is the capital of north korea?",
+      answers: ["pyongyang","tokyo"]
+      correctAnswer: "pyongyang"},
+      {text: "what is the capital of japan?",
+      answers: ["pyongyang","tokyo"]
+      correctAnswer: "tokyo"},
+  ]
+  
+    //For loop that creates buttons
+    for (var i = 0; i < answers.length; currentQuestion++)
+    document.querySelector("#answers").innerHTML += "<button>"+answers[i]+"</button>"
+    innerHTML = ""
+  
+    //When submitted, if answer correct, currentQuestion++ if wrong timer -10 seconds
+    
+    function userAnswer () {
+    var correctAnswer = questions[currentIndex].correctAnswer;
+    var outcome = document.getElementById("outcome");
+    if (Answers === correctAnswer) {
+        outcome.innerHTML = 'Correct!';
+    return currentQuestion++
+    }
+    else {
+        outcome.innerHTML = "Wrong!";
+        timer = timer - 10;
+        currentQuestion++}
+      }
+  
