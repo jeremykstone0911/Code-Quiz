fd9d9f0f7a50fe8cef5bbbf03f02e6239cf92cab
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-06-11 16:16:26 -0500

    Created new html files for High Scores Page and a Quiz Page

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,66 @@
-
-
-
-
 //Welcome page with message and start button
+
+//Q&A page
+
+var currentQuestion = 0;
+var timer = 75;
+
+var questions = [
+  {
+    text: "Is coding fun?",
+    answers: ["Yes", "No"],
+    correctAnswer: "Yes",
+  },
+  {
+    text: "Is coding fun?",
+    answers: ["Yes", "No"],
+    correctAnswer: "Yes",
+  },
+  {
+    text: "Is coding fun?",
+    answers: ["Yes", "No"],
+    correctAnswer: "Yes",
+  },
+  {
+    text: "Is coding fun?",
+    answers: ["Yes", "No"],
+    correctAnswer: "Yes",
+  },
+  {
+    text: "Is coding fun?",
+    answers: ["Yes", "No"],
+    correctAnswer: "Yes",
+  },
+];
+
+var questionText = questions[currentQuestion].text;
+var answers = questions[currentQuestion].answers;
+var correctAnswer = questions[currentQuestion].correctAnswer;
+
 document.querySelector("#startButton").addEventListener("click", function () {
   document.querySelector("#welcomeDiv").classList.add("hide");
-  });
-  
-  //Q&A page
-  document.querySelector("#questionsDiv")
-  var currentQuestion = 0
-  var timer = 75
-  var answers = questions[currentQuestion].answers
-  var correctAnswer = questions[currentQuestion].correctAnswer
-  var questions = [
-      {text: "what is the capital of north korea?",
-      answers: ["pyongyang","tokyo"]
-      correctAnswer: "pyongyang"},
-      {text: "what is the capital of japan?",
-      answers: ["pyongyang","tokyo"]
-      correctAnswer: "tokyo"},
-      {text: "what is the capital of north korea?",
-      answers: ["pyongyang","tokyo"]
-      correctAnswer: "pyongyang"},
-      {text: "what is the capital of japan?",
-      answers: ["pyongyang","tokyo"]
-      correctAnswer: "tokyo"},
-  ]
-  
-    //For loop that creates buttons
-    for (var i = 0; i < answers.length; currentQuestion++)
-    document.querySelector("#answers").innerHTML += "<button>"+answers[i]+"</button>"
-    innerHTML = ""
-  
-    //When submitted, if answer correct, currentQuestion++ if wrong timer -10 seconds
-    
-    function userAnswer () {
-    var correctAnswer = questions[currentIndex].correctAnswer;
-    var outcome = document.getElementById("outcome");
-    if (Answers === correctAnswer) {
-        outcome.innerHTML = 'Correct!';
-    return currentQuestion++
-    }
-    else {
-        outcome.innerHTML = "Wrong!";
-        timer = timer - 10;
-        currentQuestion++}
-      }
-  
+  document.querySelector("#questionsDiv").innerHTML +=
+    "<h3>Question #1</h3><h5>" + questionText + "</h5><div id='answers'></div>";
+  for (var i = 0; i < answers.length; i++) {
+    document.querySelector("#answers").innerHTML +=
+      "<button class='answerButton'>" + answers[i] + "</button>";
+  }
+  document
+    .querySelector(".answerButton")
+    .addEventListener("click", function () {});
+});
+
+//When submitted, if answer correct, currentQuestion++ if wrong timer -10 seconds
+
+function userAnswer() {
+  var correctAnswer = questions[currentIndex].correctAnswer;
+  var outcome = document.getElementById("outcome");
+  if (Answers === correctAnswer) {
+    outcome.innerHTML = "Correct!";
+    return currentQuestion++;
+  } else {
+    outcome.innerHTML = "Wrong!";
+    timer = timer - 10;
+    currentQuestion++;
+  }
+}
