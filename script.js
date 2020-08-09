a2ab15c013041e08d2af3b1eff83b6bea4a9cd98
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-08 23:29:54 -0500

    functions completed for cycling through questions

--- script.js
+++ script.js
@@ -69,3 +69,112 @@ let question5 = {
   answer4: "This is not the right answer",
   correctAnswer: "answerOne",
 };
+
+//Questions stored in an array
+let questions = [question1, question2, question3, question4, question5];
+
+let questioN = document.querySelector("#question");
+let firstAnswer = document.querySelector("#answer1");
+let secondAnswer = document.querySelector("#answer2");
+let thirdAnswer = document.querySelector("#answer3");
+let fourthAnswer = document.querySelector("#answer4");
+let answers = document.querySelector("#answers");
+
+let questionIndex = 0;
+
+//Display the questions on the page
+questioN.textContent = questions[questionIndex].question;
+firstAnswer.textContent = questions[questionIndex].answer1;
+secondAnswer.textContent = questions[questionIndex].answer2;
+thirdAnswer.textContent = questions[questionIndex].answer3;
+fourthAnswer.textContent = questions[questionIndex].answer4;
+
+// Declaring a variable to select for the element that will display if they answered the question correctly
+let printResult = document.querySelector("#result");
+
+// Click handler function
+function handleClick(event) {
+  // If they choose the first answer
+  if (event.target.matches("#answer1")) {
+    // Determining whether or not the answer is correct or not
+    if (questions[questionIndex].correctAnswer === "answerOne") {
+      // print correct if it's the correct answer
+      printResult.textContent = "Correct!";
+      // make a call back function to remove the message after 1 second
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+    } else {
+      printResult.textContent = "Incorrect! -10s";
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+      // If the answer is not correct remove time from the timer
+      secondsLeft = secondsLeft - 10;
+    }
+
+    // Repeat for all the other  possible answers
+  } else if (event.target.matches("#answer2")) {
+    if (questions[questionIndex].correctAnswer === "answerTwo") {
+      printResult.textContent = "Correct!";
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+    } else {
+      printResult.textContent = "Incorrect! -10s";
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+      secondsLeft = secondsLeft - 10;
+    }
+  } else if (event.target.matches("#answer3")) {
+    if (questions[questionIndex].correctAnswer === "answerThree") {
+      printResult.textContent = "Correct!";
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+    } else {
+      printResult.textContent = "Incorrect! -10s";
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+      secondsLeft = secondsLeft - 10;
+    }
+  } else if (event.target.matches("#answer4")) {
+    if (questions[questionIndex].correctAnswer === "answerFour") {
+      printResult.textContent = "Correct!";
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+    } else {
+      printResult.textContent = "Incorrect! -10s";
+      setTimeout(function () {
+        printResult.textContent = "";
+      }, 1000);
+      secondsLeft = secondsLeft - 10;
+    }
+  }
+  // Update the question index for a new question after they pick an answer
+  questionIndex += 1;
+  // Check to see if there are more questions left in the array
+  // If there is another one, then the DOM is updated with that question
+  if (questionIndex < questions.length) {
+    questioN.textContent = questions[questionIndex].question;
+    firstAnswer.textContent = questions[questionIndex].answer1;
+    secondAnswer.textContent = questions[questionIndex].answer2;
+    thirdAnswer.textContent = questions[questionIndex].answer3;
+    fourthAnswer.textContent = questions[questionIndex].answer4;
+    // If secondsLeft becomes negative because of the time penalty, then it is change to 0
+  } else {
+    if (secondsLeft < 0) {
+      secondsLeft = 0;
+    }
+    // The time key is updated with the new value after the penalty
+    localStorage.setItem("time", secondsLeft);
+    // Redirected to the score submission page
+    window.location.href = "score.html";
+  }
+}
+
+// Event listener for click event
+answers.addEventListener("click", handleClick);
