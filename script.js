9d5f54de01f105f09bcb37b15b2b263ea60ec611
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-08 23:02:24 -0500

    Q&A's completed on script.js

--- script.js
+++ script.js
@@ -23,3 +23,49 @@ function quizTimer() {
 
 // Call the quizTimer function
 quizTimer();
+
+//Questions and answers stored in objects
+
+let question1 = {
+  question: "1.  Which of the below is NOT a programming language?",
+  answer1: "Javascript",
+  answer2: "English",
+  answer3: "C++",
+  answer4: "Python",
+  correctAnswer: "answerTwo",
+};
+
+let question2 = {
+  question: "2.  If/Else statements are used for what?",
+  answer1: "Selecting a random piece of data.",
+  answer2: "To help indecisive people figure out what to have for dinner.",
+  answer3: 'To provide a "choice": if this do that, if not else do this.',
+  answer4: "Sorting numbers by even or odd.",
+  correctAnswer: "answerThree",
+};
+let question3 = {
+  question: "3.  For loops are used for what?",
+  answer1: "Breakfast",
+  answer2:
+    "To repeat a command until the specified condition is no longer true",
+  answer3: "To create a never ending cycle ",
+  answer4: "To repeat a command the specified number of times",
+  correctAnswer: "answerFour",
+};
+let question4 = {
+  question: "4.  While loops are used for what?",
+  answer1: "A healthy snack",
+  answer2: "To repeat a command a specified number of times",
+  answer3:
+    "To repeat a command until the specified condition is no longer true",
+  answer4: "Taking a break while long scripts run",
+  correctAnswer: "answerThree",
+};
+let question5 = {
+  question: "5.  Is coding fun?",
+  answer1: "Heck Yes!",
+  answer2: "Nah",
+  answer3: "It is okay!",
+  answer4: "This is not the right answer",
+  correctAnswer: "answerOne",
+};
